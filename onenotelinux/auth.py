97bf61696e748940219607a6bff763627feae419
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-09-09 22:48:58 -0700

    Added basic html parsing and indexing. Have to complete by walking all the fields

--- onenotelinux/auth.py
+++ onenotelinux/auth.py
@@ -7,40 +7,38 @@ import logging
 from pathlib import Path
 from typing import List
 
-CACHE_PATH = Path(os.path.join(Path.home(), ".cache", "onenotelinux", "user_token.bin"))
+CACHE_PATH = Path(os.path.join(Path.home(), ".onenotelinux", "user_token.bin"))
 
 logger = logging.getLogger(__name__)
 
 
-def _serialize_cache(cache: msal.TokenCache):
-    if cache.has_state_changed:
-        cache_dir = CACHE_PATH.parent
-        if not cache_dir.exists():
-            os.makedirs(cache_dir)
-        open(CACHE_PATH, "w").write(cache.serialize())
-
-
 class OneNoteAuthenticator():
-    def __init__(self, user_name: str, client_id: str, scopes: List[str], token_cache: msal.TokenCache):
-        self.scopes = scopes
+    def __init__(self, user_name: str, client_id: str, scopes: List[str]):
+        self.cache = msal.SerializableTokenCache()
+        if CACHE_PATH.exists():
+            self.cache.deserialize(open(CACHE_PATH, "r").read())
+        atexit.register(self._serialize_cache)
+
+        self.user_name = user_name
         self.app = msal.PublicClientApplication(
             client_id=client_id,
-            token_cache=cache
+            token_cache=self.cache
         )
+        self.scopes = scopes
 
     def __call__(self):
         """Uses device code flow
         Authorization code flow is annoying because it is meant to be used in servers which should be 
         time synced. Else, the token barfs because the time is slightly off when msal tries to decode """
-        accounts = self.app.get_accounts(user_name)
+        accounts = self.app.get_accounts(self.user_name)
         result = None
         if accounts:
             logger.debug("account exists. User must have logged-in before. Taking the first account")
-            result = self.app.acquire_token_silent(scopes, account=accounts[0])
+            result = self.app.acquire_token_silent(self.scopes, account=accounts[0])
         if not result:
             logger.debug("Could not get the credentials. Using device flow")
 
-            flow = self.app.initiate_device_flow(scopes=scopes)
+            flow = self.app.initiate_device_flow(scopes=self.scopes)
             print(flow["message"])
             result = self.app.acquire_token_by_device_flow(flow)
 
@@ -48,6 +46,13 @@ class OneNoteAuthenticator():
             raise Exception(result["error_description"])
 
         return result["access_token"]
+
+    def _serialize_cache(self):
+        if self.cache.has_state_changed:
+            cache_dir = CACHE_PATH.parent
+            if not cache_dir.exists():
+                os.makedirs(cache_dir)
+            open(CACHE_PATH, "w").write(self.cache.serialize())
 
 
 class OneNoteSession(requests.Session):
@@ -70,7 +75,7 @@ class OneNoteSession(requests.Session):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     # print(get_access_device_flow())
     client_id = "543ead0b-cc06-487c-9b75-67213f2d5fff"
@@ -78,10 +83,8 @@ if __name__ == '__main__':
     redirect_uri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
     user_name = "antonydeepak@gmail.com"
 
-    cache = msal.SerializableTokenCache()
-    if CACHE_PATH.exists():
-        cache.deserialize(open(CACHE_PATH, "r").read())
-    atexit.register(_serialize_cache, cache)
-
-    authenticator = OneNoteAuthenticator(user_name, client_id, scopes, cache)
+    authenticator = OneNoteAuthenticator(user_name, client_id, scopes)
     s = OneNoteSession(authenticator)
+    r = s.get("https://graph.microsoft.com/v1.0/me/onenote/notebooks")
+    r.raise_for_status()
+    print(r.text)
--- onenotelinux/walk.py
+++ onenotelinux/walk.py
@@ -1,6 +1,79 @@
-import requests
+import logging
+import os
+import subprocess
 
-# get all the notebooks
-requests.Session()
+from auth import OneNoteAuthenticator, OneNoteSession
+from html.parser import HTMLParser
+from io import StringIO
+from pathlib import Path
+# check if session works
+# get all the notebooks pages locally
+
+# parse the response into title, content, url. Dont' store content, but store title and url. index content and title
+# use the schema to call tantivy for indexing
+
+# Get all pages with https://graph.microsoft.com/v1.0/me/onenote/pages
+# Keep using the nextLink to navigate to next pages
+
+client_id = "543ead0b-cc06-487c-9b75-67213f2d5fff"
+scopes = ["user.read", "notes.read"]
+redirect_uri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
+user_name = "antonydeepak@gmail.com"
 
 
+class HtmlOnenoteContentParse(HTMLParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.is_ptag = False
+        self._content = []
+
+    def handle_starttag(self, tag, attrs):
+        self.is_ptag = (tag == "p")
+
+    def handle_data(self, data):
+        if self.is_ptag:
+            self._content.append(data)
+
+    @property
+    def content(self):
+        return " ".join(self._content)
+
+
+authenticator = OneNoteAuthenticator(user_name, client_id, scopes)
+s = OneNoteSession(authenticator)
+content_parser = HtmlOnenoteContentParse()
+INDEX_DIR_PATH = Path(os.path.join(Path.home(), ".onenotelinux", "index"))
+if not INDEX_DIR_PATH.exists():
+    os.makedirs(INDEX_DIR_PATH)
+
+
+def index(page_url):
+    c = s.get(page_url)
+    content_parser.feed(c.text)
+    c = content_parser.content
+    p = subprocess.run(["tantivy", "index", "--index", INDEX_DIR_PATH], input=c, encoding="utf8")
+    print(p.returncode)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    # print(get_access_device_flow())
+    # client_id = "543ead0b-cc06-487c-9b75-67213f2d5fff"
+    # scopes = ["user.read", "notes.read"]
+    # redirect_uri = "https://login.microsoftonline.com/common/oauth2/nativeclient"
+    # user_name = "antonydeepak@gmail.com"
+
+    # authenticator = OneNoteAuthenticator(user_name, client_id, scopes)
+    # s = OneNoteSession(authenticator)
+    # r = s.get("https://graph.microsoft.com/v1.0/users/me/onenote/pages/0-a03623f19fed436ebc0657fddc45a2fd!19-F33E492FF42DEBDD!324082/content")
+    # r.raise_for_status()
+    # p = open("page.html", "w")
+    # p.write(r.text)
+    # p.flush()
+
+    # p = HtmlOnenoteContentParse()
+    # p.feed(open("page.html", "r").read())
+    # print(p.content)
+    index("https://graph.microsoft.com/v1.0/users/me/onenote/pages/0-a03623f19fed436ebc0657fddc45a2fd!19-F33E492FF42DEBDD!324082/content")
