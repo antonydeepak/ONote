1ff7a66e518590eb47f062bb9f57dab6ae6a766b
Antony Deepak Thomas <antonydeepak@gmail.com>
2020-09-10 23:16:40 -0700

    Finished first version of indexing.
    TODO:
    Have to enumerate multiple pages
    Have to make it concurrent

--- onenotelinux/walk.py
+++ onenotelinux/walk.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import subprocess
@@ -14,6 +15,7 @@ from pathlib import Path
 
 # Get all pages with https://graph.microsoft.com/v1.0/me/onenote/pages
 # Keep using the nextLink to navigate to next pages
+# from the /pages extract the content url & title
 
 client_id = "543ead0b-cc06-487c-9b75-67213f2d5fff"
 scopes = ["user.read", "notes.read"]
@@ -42,22 +44,44 @@ class HtmlOnenoteContentParse(HTMLParser):
 
 authenticator = OneNoteAuthenticator(user_name, client_id, scopes)
 s = OneNoteSession(authenticator)
-content_parser = HtmlOnenoteContentParse()
 INDEX_DIR_PATH = Path(os.path.join(Path.home(), ".onenotelinux", "index"))
 if not INDEX_DIR_PATH.exists():
     os.makedirs(INDEX_DIR_PATH)
+PAGES_URL = "https://graph.microsoft.com/v1.0/me/onenote/pages"
 
 
-def index(page_url):
-    c = s.get(page_url)
-    content_parser.feed(c.text)
-    c = content_parser.content
-    p = subprocess.run(["tantivy", "index", "--index", INDEX_DIR_PATH], input=c, encoding="utf8")
-    print(p.returncode)
+def index_pages():
+    logging.debug("Fetching pages")
+    r = s.get(PAGES_URL)
+    r.raise_for_status()
+
+    c = json.loads(r.text)
+    pages = c["value"]
+    indexable_pages = []
+    for page in pages:
+        title = page["title"]
+
+        content_url = page["contentUrl"]
+        r = s.get(content_url)
+        content_parser = HtmlOnenoteContentParse()
+        content_parser.feed(r.text)
+        content = content_parser.content
+
+        url = page["links"]["oneNoteWebUrl"]["href"]
+
+        indexable_pages.append(json.dumps({
+            "title": title,
+            "content": content,
+            "url": url
+        }))
+    logging.debug(f"first page {indexable_pages[0]}")
+    pages = "\n".join(indexable_pages)
+    p = subprocess.run(["tantivy", "index", "--index", INDEX_DIR_PATH], input=pages, encoding="utf8")
+    return p.returncode
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
 
     # print(get_access_device_flow())
     # client_id = "543ead0b-cc06-487c-9b75-67213f2d5fff"
@@ -76,4 +100,5 @@ if __name__ == '__main__':
     # p = HtmlOnenoteContentParse()
     # p.feed(open("page.html", "r").read())
     # print(p.content)
-    index("https://graph.microsoft.com/v1.0/users/me/onenote/pages/0-a03623f19fed436ebc0657fddc45a2fd!19-F33E492FF42DEBDD!324082/content")
+    # index("https://graph.microsoft.com/v1.0/users/me/onenote/pages/0-a03623f19fed436ebc0657fddc45a2fd!19-F33E492FF42DEBDD!324082/content")
+    print(index_pages())
